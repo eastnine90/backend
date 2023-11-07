d1712e5325277e278cb983ec3474e513b26e847f
toutelajourn6e <toutelajourn6e@gmail.com>
2023-11-07 14:22:45 +0900

    Created Model Serializer

--- career_site/career_site/settings.py
+++ career_site/career_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data.apps.DataConfig',
+    'data_api.apps.DataApiConfig',
 ]
 
 MIDDLEWARE = [
